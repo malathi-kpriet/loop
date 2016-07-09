4c48461c3626ca2aa4fd62d3ec7ae5618d7a8d6c
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-09 15:26:06 +0530

    finish

---
+++ README.java
@@ -0,0 +1,16 @@
+# loop
+class loop
+{
+public static void main(strig args[])
+{
+int a=2;
+int b=3;
+int c=4;
+int add;
+int i;
+for(i=0;i<=3;i++)
+{
+add=add+i;
+}
+System.out.println("add"+add);
+}
